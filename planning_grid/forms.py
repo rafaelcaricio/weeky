e8a32f2d5332c1e8b4f7a44953a181ec95937e13
Rafael Caricio <rafael.jacinto@gmail.com>
2011-07-08 00:31:24 -0300

    Finalization of widgets for week plannning. The bases for start js and css work.

--- planning_grid/forms.py
+++ planning_grid/forms.py
@@ -1,17 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import calendar
+
+from django.conf import settings
 from django import forms
 
+PERIODS_OF_DAY = getattr(settings, u'PERIODS_OF_DAY', (u'Morning', u'Midday', u'Evening'))
 
 class PlanningCellWidget(forms.Textarea):
-    def __init__(self, *args, **kwargs):
-        super(PlanningCellWidget, self).__init__(*args, **kwargs)
+    def __init__(self, period_of_day, week_day, **kwargs):
+        super(PlanningCellWidget, self).__init__(**kwargs)
+        self.period_of_day = period_of_day
+        self.week_day = week_day
+
+    @property
+    def cell_name(self):
+        return u'%s_%s' % (self.week_day[:3], self.period_of_day)
+
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        attrs = super(PlanningCellWidget, self).build_attrs(extra_attrs, **kwargs)
+
+        attrs[u'id'] = u'%s_id' % self.cell_name.lower()
+
+        if not u'name' in attrs:
+            attrs[u'name'] = self.cell_name
+
+        return attrs
 
 class PlanningGridWidget(forms.MultiWidget):
-    def __init__(self, attrs={}):
-        # create the widgets
-        super(PlanningGridWidget, self).__init__(widgets, attrs)
 
-class MultipleChoiceGridField(forms.MultipleChoiceField):
-    pass
+    class Media:
+        css = {
+                "all": "css/planning_grid.css",
+        }
+
+        js = ("js/planning_grid.js",)
+
+    def __init__(self, week_starts_in=calendar.MONDAY, attrs={}):
+        self.planning_cells = []
+        self.planning_calendar = calendar.Calendar(week_starts_in)
+
+        for period_of_day in PERIODS_OF_DAY:
+            for week_day in self.planning_calendar.iterweekdays():
+                self.planning_cells.append(PlanningCellWidget(period_of_day, calendar.day_name[week_day], attrs=attrs))
+
+        super(PlanningGridWidget, self).__init__(self.planning_cells, attrs)
+
+    def format_output(self, rendered_widgets):
+        sequence = 0
+        planning_table = [u'<table><tr><th></th>']
+
+        for week_day in self.planning_calendar.iterweekdays():
+            planning_table.append(u'<th>%s</th>' % calendar.day_name[week_day][:3])
+        planning_table.append(u'</tr>')
+
+        for period_of_day in PERIODS_OF_DAY:
+            planning_table.append(u'<tr><th>%s</th>' % period_of_day)
+            for week_day in self.planning_calendar.iterweekdays():
+                planning_table.append(u'<td>%s</td>' % rendered_widgets[sequence])
+                sequence += 1
+            planning_table.append(u'</tr>')
+
+        planning_table.append(u'</table>')
+        return u''.join(planning_table)
+
+    def value_from_datadict(self, data, files, name):
+        value = {}
+        for i, widget in enumerate(self.widgets):
+            value[widget.cell_name] = widget.value_from_datadict(data, files, name + '_%s' % i)
+        return value 
--- planning_grid/tests.py
+++ planning_grid/tests.py
@@ -1,12 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from xml.etree import ElementTree
+
 from django.test import TestCase
 
-from planning_grid.forms import PlanningCellWidget
+from planning_grid.forms import PlanningCellWidget, PlanningGridWidget
 
 class PlanningCellWidgetTest(TestCase):
 
     def test_setting_custom_attributes(self):
-        cell = PlanningCellWidget(attrs={'id': 'my_custom_id'})
-        self.assertTrue('id="my_custom_id"' in cell.render("casa", ""))
+        cell = PlanningCellWidget('Morning', 'Sunday', attrs={'bla': 'my_custom_attr'})
+        element = ElementTree.fromstring(cell.render("casa", ""))
+        self.assertEquals(element.get("bla"), "my_custom_attr")
+
+    def test_create_attrs_relative_to_params(self):
+        cell = PlanningCellWidget('Morning', 'Sunday', attrs={'bla': 'my_custom_attr'})
+        attrs = cell.build_attrs()
+        self.assertEqual(attrs['id'], 'sun_morning_id')
+        self.assertEqual(attrs['name'], 'Sun_Morning')
+
+    def test_use_field_name_when_passed_but_not_id(self):
+        cell = PlanningCellWidget('Midday', 'Monday', attrs={
+            "id": "not_used",
+            "name": "use_this_name"
+        })
+        attrs = cell.build_attrs()
+        self.assertEqual(attrs['id'], 'mon_midday_id')
+        self.assertEqual(attrs['name'], 'use_this_name')
+
+class PlanningGridWidgetTest(TestCase):
+
+    def test_creation_of_widget(self):
+        plan = PlanningGridWidget()
+        self.assertTrue(plan)
+        self.assertEquals(len(plan.planning_cells), 21, "The number of cells is not 21")
+
+    def test_plan_rendering_output(self):
+        plan = PlanningGridWidget()
+        element = ElementTree.fromstring(plan.render("week_planning", []))
+        #print plan.render("week_plan", [])
+
+        self.assertEquals(element.tag, "table", "should be use a table to store the cells of planning")
+
+        first_tr = element.find('tr')
+        plan_week_days = first_tr.findall('th')[1:]
+
+        self.assertEquals(len(plan_week_days), 7, "should be 7 week days")
+        self.assertEquals(plan_week_days[0].text, "Mon", "the first day of week should be Mon")
+        self.assertEquals(plan_week_days[-1].text, "Sun", "the last day of week should be Sun")
+
+        periods_of_day = element.findall('tr')[1:]
+
+        self.assertEquals(len(periods_of_day), 3, "should be 3 periods of day")
+        self.assertEquals(periods_of_day[0].find('th').text, "Morning", "should be 'Morning'")
+        self.assertEquals(periods_of_day[1].find('th').text, "Midday", "should be 'Midday'")
+        self.assertEquals(periods_of_day[2].find('th').text, "Evening", "should be 'Evening'")
+
+    def test_plan_from_data(self):
+        plan = PlanningGridWidget()
+        data_dict = {}
+        for i in xrange(21):
+            data_dict["week_plan_%d" % i] = i == 0 and "Gym" or str(i)
+
+        value = plan.value_from_datadict(data_dict, None, "week_plan")
+
+        self.assertEquals(type(value), dict, "should be a dict")
+        self.assertTrue("Mon_Morning" in value, "should have the 'Mon_Morning' key in result value")
+        self.assertEquals(value["Mon_Morning"], "Gym", "should have the 'Gym' at 'Mon_Morning'")
+        self.assertEquals(len(value.keys()), 21, "should have 21 keys for activities")
+
