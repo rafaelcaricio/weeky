097aaa8376f74f2d32f64140455d6707681563a8
Rafael Caricio <rafael.jacinto@gmail.com>
2011-07-09 10:32:28 -0300

    mootools in the template and fix bug in media paths in settings.

--- sample_proj/settings.py
+++ sample_proj/settings.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = join(ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = join(ROOT_PATH, 'media')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -74,6 +74,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # Additional locations of static files
 STATICFILES_DIRS = (
 
+    join(ROOT_PATH, 'media'),
     abspath(join(ROOT_PATH, '../planning_grid/media')),
 
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
